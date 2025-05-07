493eb24a25cb5f47ec8655a9ef33b74f9e796571
rashidridha <rashidfirhat98@gmail.com>
2025-05-07 14:05:41 +0800

    Resolve build issue

--- src/lib/utils/getBase64.ts
+++ src/lib/utils/getBase64.ts
@@ -1,13 +1,6 @@
 import { getPlaiceholder } from "plaiceholder";
 import type { Photo } from "@/models/Images";
 
-type BlurredDataUrlArgs = {
-  blurredDataUrl?: string | null;
-  src: {
-    large: string;
-  };
-};
-
 async function getBase64(imageUrl: string) {
   try {
     const res = await fetch(imageUrl);
@@ -27,20 +20,18 @@ async function getBase64(imageUrl: string) {
 }
 
 export default async function addBlurredDataUrls(
-  images: Photo[] | BlurredDataUrlArgs[]
-): Promise<Photo[] | BlurredDataUrlArgs[]> {
+  images: Photo[]
+): Promise<Photo[]> {
   // Make all requests at once instead of awaiting each one - avoiding a waterfall
   const base64Promises = images.map((photo) => getBase64(photo.src.large));
 
   // Resolve all requests in order
   const base64Results = await Promise.all(base64Promises);
 
-  const photosWithBlur: Photo[] | BlurredDataUrlArgs[] = images.map(
-    (photo, i) => {
-      photo.blurredDataUrl = base64Results[i];
-      return photo;
-    }
-  );
+  const photosWithBlur: Photo[] = images.map((photo, i) => {
+    photo.blurredDataUrl = base64Results[i];
+    return photo;
+  });
 
   return photosWithBlur;
 }
